eee925b180e01eb668a9042d1a0d32c71ef55441
AnouarSedki <149372795+AnouarSedki@users.noreply.github.com>
2025-03-18 09:03:27 +0100

    Commit avancé

--- MediaPlayer/PG/AfficherGraphe.c
+++ MediaPlayer/PG/AfficherGraphe.c
@@ -7,11 +7,11 @@
 void equationSecondDegré(double a,double b,double c) {
     
     FILE *gnuplot = popen("gnuplot -persist","w");
-    fprintf(gnuplot, "set title 'Polynôme du second degré'\n");
+
+    fprintf(gnuplot, "set title 'Gaphe du second degré'\n");
     fprintf(gnuplot, "set xlabel 'x'\n");
     fprintf(gnuplot, "set ylabel 'f(x)'\n");
-    fprintf(gnuplot, "plot %lf*x**2 + %lf*x + %lf title 'f(x) = %.2lfx² + %.2lfx + %.2lf' with lines\n", a, b, c, a, b, c);
-    fflush(gnuplot);
+    fprintf(gnuplot, "plot %lf*x**2 + %lf*x + %lf title 'f(x) = %.2lfx² + %.2lfx + %.2lf' with lines\n", a, b, c, a, b, c); 
     pclose(gnuplot);
 
 }
@@ -19,16 +19,16 @@ void equationSecondDegré(double a,double b,double c) {
 
 int main(void){
 
-    int a;
-    int b;
-    int c;
+    double a;
+    double b;
+    double c;
 
     printf("Entrez le premier terme du polynome du second degrés\n");
-    scanf("%d",&a);
+    scanf("%lf",&a);
     printf("Entrez le deuxieme terme du polynome du second degrés\n");
-    scanf("%d",&b);
+    scanf("%lf",&b);
     printf("Entrez le troisième terme du polynome du second degrés\n");
-    scanf("%d",&c);
+    scanf("%lf",&c);
 
     equationSecondDegré(a,b,c);
 
