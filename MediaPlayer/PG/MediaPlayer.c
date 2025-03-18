9bcb21eea5ccbcbf8f0b92cf681b256d80db7bb6
AnouarSedki <149372795+AnouarSedki@users.noreply.github.com>
2025-03-18 10:46:26 +0100

    Mediaplayer 2

--- MediaPlayer/PG/MediaPlayer.c
+++ MediaPlayer/PG/MediaPlayer.c
@@ -1,1 +1,14 @@
-go
+#include "stdio.h"
+#include "stdlib.h"
+
+
+void AfficherImage(const char *LienDuFichier) {
+    char commande[256];
+    snprintf(commande,sizeof(commande),"eog %s",LienDuFichier);
+    system(commande);
+}
+
+int main(){
+    AfficherImage("../images/image1.jpeg");
+    return 0;
+}
